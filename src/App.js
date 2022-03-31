400c4e25d46cc94761115fc47d7ece01018173b4
Max <max.chapman@cloudhouse.com>
2022-03-31 10:20:47 +0100

    checkout page in progress

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import AboutUs from "./components/Pages/AboutUs";
 //Plantshed Logo
 import logo from "./images/plantshedLogo.png";
 //App CSS
-import './App.css';
+import "./App.css";
+import Checkout from "./components/Pages/Checkout";
 
 function App() {
   return (
@@ -24,16 +25,48 @@ function App() {
                 <img src={logo} alt="Plantshed logo" />
               </Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">              
+              <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link className='products' as={Link} to="/Products" exact="true">
+                  <Nav.Link
+                    className="products"
+                    as={Link}
+                    to="/Products"
+                    exact="true"
+                  >
                     Products
                   </Nav.Link>
-                  <Nav.Link className='countries' as={Link} to="/Countries" exact="true">
+                  <Nav.Link
+                    className="countries"
+                    as={Link}
+                    to="/Countries"
+                    exact="true"
+                  >
                     Countries
                   </Nav.Link>
-                  <Nav.Link className='aboutus' as={Link} to="/AboutUs" exact="true">
+                  <Nav.Link
+                    className="aboutus"
+                    as={Link}
+                    to="/AboutUs"
+                    exact="true"
+                  >
                     About Us
+                  </Nav.Link>
+                  <Nav.Link
+                    className="checkout"
+                    as={Link}
+                    to="/Checkout"
+                    exact="true"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-cart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
                   </Nav.Link>
                 </Nav>
               </Navbar.Collapse>
@@ -46,6 +79,7 @@ function App() {
             <Route path="/Products" element={<Products />} exact />
             <Route path="/Countries" element={<Countries />} exact />
             <Route path="/AboutUs" element={<AboutUs />} exact />
+            <Route path="/Checkout" element={<Checkout />} exact />
           </Routes>
         </div>
       </div>
