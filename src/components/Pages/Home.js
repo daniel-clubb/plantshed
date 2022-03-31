ff5c0d699c27d0d21ba5d464608b81ace7bb04ec
daniel-clubb <danclubb004@gmail.com>
2022-03-31 11:39:59 +0100

    moved filter component to products page and removed filter from homepage

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Filter from "../Filter";
 
 export default function Home() {
   return (
     <>
       <div>
         <h1>HomePage</h1>
-        <Filter />
       </div>
     </>
   );
